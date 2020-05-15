d6997b6c9490271a3f8e8d8a6a01988cf7ed7b35
Paulo Oliveira <pco@dmz.org.br>
2020-05-15 14:54:52 -0300

    Debug
    Adding debug info

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 api = Api(app)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(debug=True)
 
 api.add_resource(BookingList, "/booking/")
--- resources/bookings.py
+++ resources/bookings.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from resources.data import BOOKINGS
+import json
 
 class BookingList(Resource):
     def get(self):
@@ -16,6 +17,19 @@ class BookingList(Resource):
         parser.add_argument("additional_needs")
         args =parser.parse_args()
         booking_id = int(max(BOOKINGS.keys())) + 1
+        json_ptr = {
+            "first_name": args["first_name"],
+            "last_name": args["last_name"],
+            "total_price": args["total_price"],
+            "deposit_paid": args["deposit_paid"],
+            "checkin": args["checkin"],
+            "checkout": args["checkout"],
+            "additional_needs": args["additional_needs"]
+        }
+        print("booking_id: " + booking_id)
+        print(json.dumps(json_ptr.__dict__))
+
+
         BOOKINGS[booking_id] = {
             "first_name": args["first_name"],
             "last_name": args["last_name"],
