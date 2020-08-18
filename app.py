3c893567ef95a08fa05cf02ee285bd0aa76e7683
Paulo Oliveira <pco@dmz.org.br>
2020-08-17 23:32:25 -0300

    Revert "Refactoring code"
    This reverts commit a440ee9df43caa7cbfd8a32dbfed7d5ed8ec611e.

--- app.py
+++ app.py
@@ -1,57 +1,11 @@
-from flask import Flask, jsonify, request
-from resources.data import BOOKINGS
+from flask import Flask
+from flask_restful import Resource, Api
+from resources.bookings import BookingList
 
 app = Flask(__name__)
-app.run()
+api = Api(app)
 
-@app.route('/booking', methods=['POST'])
-def create_booking():
-    request_data = request.get_json
-    new_booking = {
-        'first_name': request_data['first_name'],
-        'last_name': request_data['last_name'],
-        'total_price': request_data['total_price'],
-        'already_paid': request_data['already_paid'],
-        'checkin': request_data['checkin'],
-        'checkout': request_data['checkout'],
-        'additional_needs': request_data['additional_needs']
-    }
-    BOOKINGS.append(new_booking)
-    return jsonify(new_booking)
+if __name__ == "__main__":
+    app.run()
 
-@app.route('/booking')
-def get_bookings():
-    return jsonify({'bookings': BOOKINGS})
-
-@app.route('/booking/<int:id>')
-def get_booking_by_id(id):
-    for book in BOOKINGS:
-        if book['id'] == id:
-            return jsonify(book)
-    return jsonify({'message': 'Booking not found!'})
-
-@app.route('/booking/<int:id>', methods='PUT')
-def edit_booking(id):
-    request_data = request.get_json
-    new_booking = {
-        'first_name': request_data['first_name'],
-        'last_name': request_data['last_name'],
-        'total_price': request_data['total_price'],
-        'already_paid': request_data['already_paid'],
-        'checkin': request_data['checkin'],
-        'checkout': request_data['checkout'],
-        'additional_needs': request_data['additional_needs']
-    }
-
-    for book in BOOKINGS:
-        if book['id'] == id:
-            book.update(new_booking)
-            return jsonify(new_booking)
-    return jsonify({'message': 'Booking not found!'})
-
-@app.route('/booking/<int:id>', methods=['DELETE'])
-def delete_booking():
-    for book in BOOKINGS:
-        if book['id'] == id:
-            BOOKINGS.remove(book)
-    return jsonify({'message': 'Booking not found!'})
+api.add_resource(BookingList, "/booking/")
