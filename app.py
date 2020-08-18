9c26515cddad404c495053b4175b53a2292062e3
Paulo Oliveira <pco@dmz.org.br>
2020-08-18 12:25:16 -0300

    Finishing booking api MVP

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from resources.bookings import BookingList
+from resources.bookings import BookingList, Booking
 
 app = Flask(__name__)
 api = Api(app)
+app.config['DEBUG'] = True
 
-if __name__ == "__main__":
-    app.run()
+api.add_resource(BookingList, "/bookings")
+api.add_resource(Booking, "/booking", endpoint="post")
+api.add_resource(Booking, "/booking/<string:first_name>")
 
-api.add_resource(BookingList, "/booking/")
+app.run()
--- resources/bookings.py
+++ resources/bookings.py
@@ -1,45 +1,58 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, request
 from resources.data import BOOKINGS
 import json
 
 class BookingList(Resource):
-    # def delete(self):
-    #     BOOKINGS.clear()
-    #     return BOOKINGS, 204
-
-    def delete(id):
-        BOOKINGS.pop(id)
-        return BOOKINGS, 204
 
     def get(self):
-        return BOOKINGS, 200
-    
-    # def get(id):
-    #     item = BOOKINGS.get(id)
-    #     return item, 200
+        return {'bookings': BOOKINGS}, 200
 
+
+class Booking(Resource):
+    
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("first_name")
-        parser.add_argument("last_name")
-        parser.add_argument("total_price")
-        parser.add_argument("deposit_paid")
-        parser.add_argument("checkin")
-        parser.add_argument("checkout")
-        parser.add_argument("additional_needs")
-        args =parser.parse_args()
-        if len(BOOKINGS) > 0:
-            booking_id = int(max(BOOKINGS.keys())) + 1
-        else:
-            booking_id = 1
-        booking_id = "%i" % booking_id
-        BOOKINGS[booking_id] = {
-            "first_name": args["first_name"],
-            "last_name": args["last_name"],
-            "total_price": args["total_price"],
-            "deposit_paid": args["deposit_paid"],
-            "checkin": args["checkin"],
-            "checkout": args["checkout"],
-            "additional_needs": args["additional_needs"]
+        request_data = request.get_json()
+        
+        if next(filter(lambda x: x['first_name'] == request_data['first_name'], BOOKINGS), None):
+            return {'message': "Booking with first name '{}' already exists".format(request_data['first_name'])}, 400
+        
+        new_booking = {
+            'first_name': request_data['first_name'],
+            'last_name': request_data['last_name'],
+            'total_price': request_data['total_price'],
+            'already_paid': request_data['already_paid'],
+            'checkin': request_data['checkin'],
+            'checkout': request_data['checkout'],
+            'additional_needs': request_data['additional_needs']
         }
-        return BOOKINGS[booking_id], 201
+        
+        BOOKINGS.append(new_booking)
+        return new_booking, 201
+    
+    def put(self, first_name):
+        request_data = request.get_json()
+
+        item = next(filter(lambda x: x['first_name'] == first_name, BOOKINGS), None);
+        if item:
+            new_booking = {
+                'first_name': request_data['first_name'],
+                'last_name': request_data['last_name'],
+                'total_price': request_data['total_price'],
+                'already_paid': request_data['already_paid'],
+                'checkin': request_data['checkin'],
+                'checkout': request_data['checkout'],
+                'additional_needs': request_data['additional_needs']
+            }
+            item.update(new_booking)
+            return new_booking, 200
+        else:
+            return {'message': "Booking with first name '{}' does not exist!".format(request_data['first_name'])}, 400
+
+    def get(self, first_name):
+        booking = next(filter(lambda x: x['first_name'] == first_name, BOOKINGS), None)
+        return {'booking': booking}, 200 if booking else 404
+    
+    def delete(self, first_name):
+        global BOOKINGS
+        BOOKINGS = list(filter(lambda x: x['first_name'] != first_name, BOOKINGS))
+        return {'message': 'Booking deleted!'}, 200
